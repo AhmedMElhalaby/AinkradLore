AhmedMElhalaby/AinkradQuest

feat(log): add os.Logger categories on the shared Ainkrad subsystem
Across all 11 Ainkrad repos, os.Logger was used in TWO files. When a user's app
misbehaves there is nothing to diagnose from. This repo had no logging at all.

Purely additive: defines the categories, adds no call sites. Converting the
repo's `try?` sites to actually log is a separate, larger task.

The subsystem string is spelled out rather than taken from the SDK's AinkradLog,
because this repo's AinkradAppKit pin (6cd1599) predates that type -- importing it
would not compile, and moving the pin would force a plugin rebuild and re-release
for a logging change. Each file says so, and says to switch when the pin next
moves forward.

Sources/QuestFeature/Log.swift
@@ -1,0 +1,16 @@
+import os
+
+/// This repo's `os.Logger` categories, all under the shared Ainkrad
+/// subsystem so a user's whole install filters as one stream in Console.app.
+///
+/// The subsystem is spelled out here rather than taken from the SDK's
+/// `AinkradLog` because this repo's AinkradAppKit pin (6cd1599) predates
+/// that type. Switch to `AinkradLog.logger(app:area:)` whenever this pin
+/// next moves forward.
+enum Log {
+    private static let subsystem = "com.ainkrad.app"
+    static let store = Logger(subsystem: subsystem, category: "quest.store")
+    static let snapshot = Logger(subsystem: subsystem, category: "quest.snapshot")
+    static let board = Logger(subsystem: subsystem, category: "quest.board")
+    static let sync = Logger(subsystem: subsystem, category: "quest.sync")
+}

AhmedMElhalaby/AinkradLore

Sources/LoreFeature/Log.swift
@@ -1,0 +1,17 @@
+import os
+
+/// This repo's `os.Logger` categories, all under the shared Ainkrad
+/// subsystem so a user's whole install filters as one stream in Console.app.
+///
+/// The subsystem is spelled out here rather than taken from the SDK's
+/// `AinkradLog` because this repo's AinkradAppKit pin (6cd1599) predates
+/// that type. Switch to `AinkradLog.logger(app:area:)` whenever this pin
+/// next moves forward.
+enum Log {
+    private static let subsystem = "com.ainkrad.app"
+    static let store = Logger(subsystem: subsystem, category: "lore.store")
+    static let editor = Logger(subsystem: subsystem, category: "lore.editor")
+    static let import_ = Logger(subsystem: subsystem, category: "lore.import")
+    static let search = Logger(subsystem: subsystem, category: "lore.search")
+    static let watcher = Logger(subsystem: subsystem, category: "lore.watcher")
+}